b35fb4e9a16f342a8c6e77fc963614eb3d6f34a0
nelotgrom <nelotgrom@gmail.com>
2019-03-23 21:27:30 +0300

    file: Complited task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 !== 0) {
+    return false;
+  } 
+  var bracket = [];
+  for(var i = 0; i < bracketsConfig.length; i++) {
+    bracket[i] = bracketsConfig[i][0] + bracketsConfig[i][1];  
+  }
+  var newstr = '';
+  while (str != newstr) {
+    newstr = str;
+    for (i = 0; i < bracket.length; i++) {
+      str = str.replace(bracket[i], '');
+    }
+  }
+  return str.length === 0 ? true : false;
 }
+
